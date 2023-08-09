4e0bedfcabd1048141b58ad7df5224ee3d022db0
Kekelberi <alexei.gaponov@gmail.com>
2023-08-08 19:00:47 -0500

    save work

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,88 @@
-use actix_web::{ get, post, web, App, HttpResponse, HttpServer, Responder };
+use actix_web::{
+    dev::ServiceRequest,
+    error::Error,
+    web::{self, Data},
+    App, HttpMessage, HttpServer,
+};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
-mod service;
+use actix_web_httpauth::{
+    extractors::{
+        bearer::{self, BearerAuth},
+        AuthenticationError,
+    },
+    middleware::HttpAuthentication,
+};
+use hmac::{Hmac, Mac};
+use jwt::VerifyWithKey;
+use serde::{Deserialize, Serialize};
+use sha2::Sha256;
+
+mod services;
+use services::{basic_auth, create_article, create_user};
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TokenClaims {
+    id: i32,
+}
+
+async fn validator(
+    req: ServiceRequest,
+    credentials: BearerAuth,
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    let jwt_secret: String = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set!");
+    let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes()).unwrap();
+    let token_string = credentials.token();
+
+    let claims: Result<TokenClaims, &str> = token_string
+        .verify_with_key(&key)
+        .map_err(|_| "Invalid token");
+
+    match claims {
+        Ok(value) => {
+            req.extensions_mut().insert(value);
+            Ok(req)
+        }
+        Err(_) => {
+            let config = req
+                .app_data::<bearer::Config>()
+                .cloned()
+                .unwrap_or_default()
+                .scope("");
+
+            Err((AuthenticationError::from(config).into(), req))
+        }
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection pool");
+
+    HttpServer::new(move || {
+        let bearer_middleware = HttpAuthentication::bearer(validator);
         App::new()
-            .service(service::start)
-            .service(service::get_user)
-            .service(service::create_user)
-            .service(service::basic_auth)
-            .service(service::create_article)
+            .app_data(Data::new(AppState { db: pool.clone() }))
+            .service(basic_auth)
+            .service(create_user)
+            .service(
+                web::scope("")
+                    .wrap(bearer_middleware)
+                    .service(create_article),
+            )
     })
-        .bind(("127.0.0.1", 3000))?
-        .run().await
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
 }
